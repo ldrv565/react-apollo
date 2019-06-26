cbf4a87c4f55e3f1e3d2d5877012e07507448267
Алейдаров Эльдар <40745264+ldrv565@users.noreply.github.com>
2019-06-26 10:29:42 +0300

    Mutation, Query

--- hackernews-react-apollo/src/components/App.js
+++ hackernews-react-apollo/src/components/App.js
@@ -1,26 +1,20 @@
 import React from 'react';
-import logo from '../logo.svg';
-import '../styles/App.css';
+import { Switch, Route } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
+import Header from './Header';
+import CreateLink from './CreateLink';
+import LinkList from './LinkList';
+
+const App = () => (
+  <div className='center w85'>
+    <Header />
+    <div className='ph3 pv1 background-gray'>
+      <Switch>
+        <Route exact path='/' component={LinkList} />
+        <Route exact path='/create' component={CreateLink} />
+      </Switch>
     </div>
-  );
-}
+  </div>
+);
 
 export default App;
--- hackernews-react-apollo/src/index.js
+++ hackernews-react-apollo/src/index.js
@@ -1,31 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './styles/index.css';
-import App from './components/App';
-import * as serviceWorker from './serviceWorker';
-
-// 1
+import { BrowserRouter } from 'react-router-dom';
 import { ApolloProvider } from 'react-apollo';
 import { ApolloClient } from 'apollo-client';
 import { createHttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 
-// 2
+import App from './components/App';
+import * as serviceWorker from './serviceWorker';
+
+import './styles/index.css';
+
 const httpLink = createHttpLink({
   uri: 'http://localhost:4000'
 });
 
-// 3
 const client = new ApolloClient({
   link: httpLink,
   cache: new InMemoryCache()
 });
 
-// 4
 ReactDOM.render(
-  <ApolloProvider client={client}>
-    <App />
-  </ApolloProvider>,
+  <BrowserRouter>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </BrowserRouter>,
   document.getElementById('root')
 );
 serviceWorker.unregister();
